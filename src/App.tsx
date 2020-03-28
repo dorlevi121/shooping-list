518217a20a5b9a3504e520adb68c8c847c6e29be
Dor <dorlevy121@gmail.com>
2020-03-28 16:31:09 +0300

    folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1>ניסיון של כותרת בעברית. עם קצת סימיני פיסוק</h1>
+     <p>זה הניסיון הראשון שלי בעברית, עם מעט סימני פיסוק.</p>
     </div>
   );
 }
